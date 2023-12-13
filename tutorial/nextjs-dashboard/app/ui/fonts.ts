a97043d4cbb8fca0685aa57b3d10d389ba61bcd8
unknown <patxi05@gmal.com>
2023-12-13 07:26:26 -0300

    optmisacion de fuentes

--- tutorial/nextjs-dashboard/app/ui/fonts.ts
+++ tutorial/nextjs-dashboard/app/ui/fonts.ts
@@ -1,4 +1,9 @@
-import {Montserrat} from 'next/font/google'
+import {Montserrat,Lusitana} from 'next/font/google'
 
 
 export const montserrat = Montserrat({subsets: ['latin']})
+
+export const lusitana = Lusitana({
+    weight : ['400','700' ] , 
+    subsets: ['latin']
+})
